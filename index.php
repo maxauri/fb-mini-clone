a7f98611b4c8b216d5816f482d8587f1f7a87226
Haseb-ali <haseb.chaudhry@gmail.com>
2021-03-31 08:59:41 +0500

    adding index.php file

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+echo "welcome to my home page";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="dist/css//home.css" />
+    <title>FB mini clone application 💡💡</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
+<script src="dist/js/home.js"></script>
